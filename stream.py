2d22687d59d725ed6e584f5f62c1771899d99844
werner1643 <werner1643@yandex.ru>
2016-09-22 22:15:28 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,17 +1,18 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*- 
 #!/usr/bin/env python
-import urllib.request
-stream_url = 'http://pub1.di.fm/di_classictrance'
+import urllib.request 
+
+stream_url = 'http://185.50.24.141:8000/main_mp3_128'
 request = urllib.request.Request(stream_url)
-try:
-    request.add_header('Icy-MetaData', 1)
-    response = urllib.request.urlopen(request)
-    icy_metaint_header = response.headers.get('icy-metaint')
-    if icy_metaint_header is not None:
-        metaint = int(icy_metaint_header)
-        read_buffer = metaint+255
-        content = response.read(read_buffer)
-        title = content[metaint:].split("'")[1]
-    print (title)
-except:
-        print ('Error')
+
+request.add_header('Icy-MetaData', 1)
+response = urllib.request.urlopen(request)
+icy_metaint_header = response.headers.get('icy-metaint')
+if icy_metaint_header is not None:
+    metaint = int(icy_metaint_header)
+    read_buffer = metaint+255
+    content = response.read(read_buffer)
+    title = content[metaint:]
+    print (title.decode('utf8', 'ignore').split("'")[1])
+    print (title.decode('utf8', 'ignore').split("'"))
+
